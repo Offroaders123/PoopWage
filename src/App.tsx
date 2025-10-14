70376a63246c8fc6ce2c8726b53d3beca5b78627
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-10-14 09:51:55 -0700

    Initial Tabbed Setup
    Going to use two panels here, one for the logging interface, the other for showing the data of the time clock itself.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
+import { For, type JSX } from "solid-js";
+import "./App.css";
+
 export default function App() {
   return (
     <>
+      <h1>PoopWage</h1>
+      <Tabbed>{[
+        ["Clock", <div>Clock interface</div>],
+        ["Log", <div>Log data</div>],
+      ]}</Tabbed>
+    </>
+  );
+}
+
+function Tabbed(props: { children: [string, JSX.Element][]; }) {
+  return (
+    <>
+      <For each={props.children}>
+        {([name, content]) => (
+          <>
+            <div>
+              <button>{name}</button>
+            </div>
+          </>
+        )}
+      </For>
     </>
   );
 }
