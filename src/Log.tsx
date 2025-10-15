f164b21b0b1403f1e0341c02b539f0528e779ca9
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-10-15 08:19:23 -0700

    Log Dates Display
    Not pretty yet really, this will get better. Might use a table or something like that. This is meant to be more like a spreadsheet of some sorts.
    
    I will add a way to download everything as JSON too.
    
    I need to add a way to remove the data too, to start fresh. To flush the database, if we were to make puns like that. Not sure who would do such a thing though.

--- src/Log.tsx
+++ src/Log.tsx
@@ -10,8 +10,34 @@ export default function Log(props: LogProps) {
   return (
     <div class="Log">
       <For each={props.logs}>
-        {log => <pre>{JSON.stringify(log)}</pre>}
+        {log => {
+          const { startTime, startDate, endTime, endDate }: LogDates = parseLogDates(log);
+          return (
+            <div>{startTime}, {startDate} | {endTime}, {endDate}</div>
+          );
+        }}
       </For>
     </div>
   );
+}
+
+interface LogDates {
+  startTime: string;
+  startDate: string;
+  endTime?: string;
+  endDate?: string;
+}
+
+function parseLogDates(log: TimeClockLog): LogDates {
+  const start: Date = new Date(log.start);
+  const startTime: string = start.toLocaleTimeString();
+  const startDate: string = start.toLocaleDateString();
+  if (typeof log.end !== "number") {
+    return { startTime, startDate };
+  }
+
+  const end: Date = new Date(log.end);
+  const endTime: string = end.toLocaleTimeString();
+  const endDate: string = end.toLocaleDateString();
+  return { startTime, startDate, endTime, endDate };
 }
